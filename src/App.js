10e2a5013d93732ce1315c6ff8ea867d8b96dd81
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-08-22 13:23:03 +0100

    Create hidden confetti button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Person from "./images/Person";
 import Project from "./components/Project";
 import BouncingText from "./components/BouncingText";
+import HiddenConfettiButton from "./components/HiddenConfettiButton";
 import { projects } from "./data";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <HiddenConfettiButton color="green" />
       <div className="landing-container">
         <div className="title">
           <h1>Jordan Renaud</h1>
@@ -27,6 +29,7 @@ function App() {
       <section>
         <h2 className="title">Easter Eggs</h2>
         <p>Hover over Developer</p>
+        <p>Click on the top left of the page</p>
       </section>
     </div>
   );
---
+++ src/components/HiddenConfettiButton.js
@@ -0,0 +1,44 @@
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
+import { useEffect, useState } from "react";
+import "../styles/HiddenConfettiButton.css";
+
+export default function HiddenConfettiButton({ color }) {
+  const { width, height } = useWindowSize();
+  const [isConfettiShown, setIsConfettiShown] = useState(false);
+  const [recycle, setRecycle] = useState(true);
+
+  useEffect(() => {
+    if (!isConfettiShown) return;
+    const timer = setTimeout(() => {
+      setRecycle(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [isConfettiShown, recycle]);
+
+  function showConfetti() {
+    setIsConfettiShown(true);
+    setRecycle(true);
+  }
+
+  return (
+    <div>
+      <button className="HiddenConfettiButton" onClick={showConfetti}></button>
+      {isConfettiShown && (
+        <Confetti
+          width={width - 40}
+          height={height * 2}
+          recycle={recycle}
+          colors={[
+            "#f2f2f2",
+            "#ffdbac",
+            "#3f3d56",
+            "#e6e6e6",
+            "#2f2e41",
+            "#6667ab",
+          ]}
+        />
+      )}
+    </div>
+  );
+}
